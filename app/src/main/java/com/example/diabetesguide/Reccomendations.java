fa5a6813a83f03934389ca9977f336c4aa727e68
Keegan <KeeganLFrank@gmail.com>
2022-10-07 17:46:05 +0200

    keegans work

---
+++ app/src/main/java/com/example/diabetesguide/Reccomendations.java
@@ -0,0 +1,14 @@
+package com.example.diabetesguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reccomendations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reccomendations);
+    }
+}
