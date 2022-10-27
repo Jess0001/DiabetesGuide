32845ab29514dc065a31b975811dd595cd2c4ab9
Keegan <KeeganLFrank@gmail.com>
2022-10-27 15:47:21 +0200

    keegans Branch

---
+++ app/src/main/java/com/example/diabetesguide/Reccomendations.java
@@ -0,0 +1,24 @@
+package com.example.diabetesguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reccomendations extends AppCompatActivity {
+
+    String LevelsDisplay;
+    int CurrentLevel;
+    int GoalLevel;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reccomendations);
+
+
+
+
+    }
+}
